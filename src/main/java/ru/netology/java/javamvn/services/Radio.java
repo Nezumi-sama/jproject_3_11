d471438633ee895108a1594fc6fdcfca01c4ed4f
Nezumi-sama <kryska007@yandex.ru>
2022-10-05 14:59:01 +0300

    Add lombok

--- src/main/java/ru/netology/java/javamvn/services/Radio.java
+++ src/main/java/ru/netology/java/javamvn/services/Radio.java
@@ -1,11 +1,20 @@
 package ru.netology.java.javamvn.services;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
     private int stationCount = 10;
     private int stationNumber = stationCount / 2;
     private int volume = 50;
 
-    public Radio(){
+
+
+/*    public Radio(){
 
     }
 
@@ -70,5 +79,5 @@ public class Radio {
         }
     }
 
-
+*/
 }
--- src/test/java/ru/netology/java/javamvn/services/RadioTest.java
+++ src/test/java/ru/netology/java/javamvn/services/RadioTest.java
@@ -7,106 +7,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
-//Test Radio constructor
     @Test
-    public void testRadioConstructorEmpty(){
+    public void shouldCreate() {
+        Radio radio = new Radio();
+    }
+
+    //Test Radio constructor
+    @Test
+    public void testRadioConstructorEmpty() {
         Radio radio = new Radio();
 
-        assertEquals(10 , radio.getStationCount());
-        assertEquals(5 , radio.getStationNumber());
-        assertEquals(50 , radio.getVolume());
+        assertEquals(10, radio.getStationCount());
+        assertEquals(5, radio.getStationNumber());
+        assertEquals(50, radio.getVolume());
     }
 
     @Test
-    public void testRadioConstructorCount(){
-        Radio radio = new Radio(13);
+    public void testRadioConstructorCount() {
+        Radio radio = new Radio(13, 6, 50);
 
-        assertEquals(13 , radio.getStationCount());
-        assertEquals(6 , radio.getStationNumber());
-        assertEquals(50 , radio.getVolume());
+        assertEquals(13, radio.getStationCount());
+        assertEquals(6, radio.getStationNumber());
+        assertEquals(50, radio.getVolume());
     }
 
 
-    // Test set StationNumber
+    // Test setters
+    @Test
+    void shouldSetStationCount() {
+        Radio radio = new Radio();
+
+        radio.setStationCount(18);
+        int actual = 18;
+        int expected = radio.getStationCount();
+        assertEquals(actual, expected);
+    }
+
     @Test
     void shouldSetStationNumber() {
         Radio radio = new Radio();
 
-        int actual = 5;
-//        radio.setStationNumber(5);
-        int expected = radio.getStationNumber();
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldSetStationNumberMore() {
-        Radio radio = new Radio();
-//        radio.setStationNumber(5);
-
-        int actual = 5;
-        radio.setStationNumber(15);
-        int expected = radio.getStationNumber();
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldSetStationNumberLess() {
-        Radio radio = new Radio();
-//        radio.setStationNumber(5);
-
-        int actual = 5;
-        radio.setStationNumber(-1);
-        int expected = radio.getStationNumber();
-        assertEquals(actual, expected);
-    }
-
-
-    @Test
-    void shouldSetNextStationNumber() {
-        Radio radio = new Radio();
-//        radio.setStationNumber(5);
-
-        int actual = 6;
-        radio.setNextStationNumber();
-        int expected = radio.getStationNumber();
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldSetNextStationNumber9() {
-        Radio radio = new Radio();
-        radio.setStationNumber(9);
-
-        int actual = 0;
-        radio.setNextStationNumber();
-        int expected = radio.getStationNumber();
-        assertEquals(actual, expected);
-    }
-
-
-    @Test
-    void shouldSetPrevStationNumber() {
-        Radio radio = new Radio();
-//        radio.setStationNumber(5);
-
+        radio.setStationNumber(4);
         int actual = 4;
-        radio.setPrevStationNumber();
         int expected = radio.getStationNumber();
         assertEquals(actual, expected);
     }
-
-    @Test
-    void shouldSetPrevStationNumber0() {
-        Radio radio = new Radio();
-        radio.setStationNumber(0);
-
-        int actual = 9;
-        radio.setPrevStationNumber();
-        int expected = radio.getStationNumber();
-        assertEquals(actual, expected);
-    }
-
-// Test set StationNumber
 
     @Test
     void shouldSetVolume() {
@@ -117,74 +62,6 @@ class RadioTest {
         int expected = radio.getVolume();
         assertEquals(actual, expected);
 
-    }
-
-    @Test
-    void shouldSetVolumeMoreMax() {
-        Radio radio = new Radio();
-//        radio.setVolume(6);
-
-        int actual = 50;
-        radio.setVolume(101);
-        int expected = radio.getVolume();
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldSetVolumeLessMin() {
-        Radio radio = new Radio();
-//        radio.setVolume(6);
-
-        int actual = 50;
-        radio.setVolume(-1);
-        int expected = radio.getVolume();
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldSetMoreVolume() {
-        Radio radio = new Radio();
-//        radio.setVolume(6);
-
-        int actual = 51;
-        radio.setMoreVolume();
-        int expected = radio.getVolume();
-        assertEquals(actual, expected);
-
-    }
-
-    @Test
-    void shouldSetMoreVolume100() {
-        Radio radio = new Radio();
-        radio.setVolume(100);
-
-        int actual = 100;
-        radio.setMoreVolume();
-        int expected = radio.getVolume();
-        assertEquals(actual, expected);
-
-    }
-
-    @Test
-    void shouldSetLessVolume() {
-        Radio radio = new Radio();
- //       radio.setVolume(6);
-
-        int actual = 49;
-        radio.setLessVolume();
-        int expected = radio.getVolume();
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void shouldSetLessVolume0() {
-        Radio radio = new Radio();
-        radio.setVolume(0);
-
-        int actual = 0;
-        radio.setLessVolume();
-        int expected = radio.getVolume();
-        assertEquals(actual, expected);
     }
 
 }
